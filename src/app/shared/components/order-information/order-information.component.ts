d2778d3ba6feb42849d5e4380644242da9df716c
JLopez <Juan.canchila@gmail.com>
2023-06-13 09:57:47 -0500

    sales person

--- src/app/shared/components/order-information/order-information.component.ts
+++ src/app/shared/components/order-information/order-information.component.ts
@@ -28,7 +28,7 @@ export class OrderInformationComponent implements OnInit, AfterContentChecked, O
   idRecord: string;
   constructor(private _lamiService: LamiService,
               private _formBuilder: FormBuilder,
-              private cdRef : ChangeDetectorRef) { 
+              private cdRef : ChangeDetectorRef) {
                 this.formGroup = this._formBuilder.group({
                   salesPersonCode: ['', Validators.required],
                   serie:  ['', Validators.required],
@@ -38,14 +38,17 @@ export class OrderInformationComponent implements OnInit, AfterContentChecked, O
   ngOnInit(): void {
 
     this.current_sales_person_code = localStorage.getItem('user_salesPerson');
-    console.log('jhkjkjk', this.idRecord, this.order)      
+
+    this.formGroup.controls.salesPersonCode.setValue(this.current_sales_person_code);
+
+    console.log('jhkjkjk', this.idRecord, this.order)
     if (!this.idRecord) {
-      this._lamiService.getU_HBT('SalesPersonCode').subscribe((result: Uhbt[]) => { 
+      this._lamiService.getU_HBT('SalesPersonCode').subscribe((result: Uhbt[]) => {
         this.salesPersonCode = result.map((item: any) => {
           return {...item, city: item.extendedData?.cities[0]};
-        });  
-      });  
-      this._lamiService.getU_HBT('SERIES').subscribe((result: Uhbt[]) => { this.Series = result });  
+        });
+      });
+      this._lamiService.getU_HBT('SERIES').subscribe((result: Uhbt[]) => { this.Series = result });
     }
 
   }
@@ -80,17 +83,17 @@ export class OrderInformationComponent implements OnInit, AfterContentChecked, O
     if (changes.order && changes.order.currentValue) {
       this.order = changes.order.currentValue;
       if (this.idRecord) {
-        this._lamiService.getU_HBT(`by-city/SalesPersonCode/${this.order?.salesPersonCode}`).subscribe((result: Uhbt[]) => { 
+        this._lamiService.getU_HBT(`by-city/SalesPersonCode/${this.order?.salesPersonCode}`).subscribe((result: Uhbt[]) => {
           this.salesPersonCode = result.map((item: any) => {
             return {...item, city: item.extendedData?.cities[0]};
-          });  
+          });
         });
-    
+
         this._lamiService.getU_HBT(`by-city/SERIES/${this.order?.salesPersonCode}`).subscribe((result: Uhbt[]) => { this.Series = result });
         this.formGroup.controls.salesPersonCode.setValue(this.order?.salesPersonCode);
         this.formGroup.controls.serie.setValue(this.order?.serie);
-  
-      } 
+
+      }
     }
   }
 
